bc7cca6d982ce6d650caa736514763e5188de186
Nick8735 <nick8723@hotmail.co.uk>
2023-08-22 21:42:47 +0000

    JS updated with some CSS for question box

--- asset/css/script/file.js
+++ asset/css/script/file.js
@@ -5,7 +5,7 @@ const questions = [
             { text: "shark", correct: false },
             { text: "fish", correct: false },
             { text: "nick", correct: true },
-            { text: "diane", correct: false },
+            { text: "goldfish", correct: false },
         ]
     },
     {    question: "who is the biggest pain?",
@@ -71,7 +71,7 @@ currentQuestion.answers.forEach(answer => {
     const button = document.createElement("button");
     button.innerHTML = answer.text;
     button.classList.add("btn");
-    answerButton.appendChild(button);
+    answerButtons.appendChild(button);
     if(answer.correct){
         button.dataset.correct = answer.correct;
     }
@@ -92,9 +92,39 @@ function selectAnswer(e){
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("correct");
+        score++;
     }else{
         selectedBtn.classList.add("incorrect");
     }
-
+    Array.from(answerButtons.children).forEach(button => {
+if(button.dataset.correct === "true"){
+button.classList.add ("correct");
 }
+button.disabled = true;
+    });
+nextButton.style.display = "block";
+}
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "play again";
+    nextButton.style.display = "block"
+}
+
+function handleNextButton(){
+currentQuestionIndex++;
+if(currentQuestionIndex < questions.length){
+showQuestion();
+}else{ 
+    showScore();
+}
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{ 
+        startQuiz(); 
+    }  
+    })
 startQuiz();
